b7f10b44b6ba4e1d9e031f6278e3710a83cdf0b4
Doikson <joseangelcortesbaillet@gmail.com>
2024-09-19 15:55:05 -0600

    Variables name changed

--- InstaFollowList.py
+++ InstaFollowList.py
@@ -4,10 +4,10 @@ import pandas as pd
 data_excel = 'data.xlsx'
 df = pd.read_excel(data_excel, sheet_name=0)
 
-Following = []
-Followers = []
-Mutual = []
-NoMatching = []
+following = []
+followers = []
+mutual = []
+no_matching = []
 
 # Create new file called results.txt
 with open('results.txt', 'w') as file:
@@ -15,28 +15,28 @@ with open('results.txt', 'w') as file:
     # Verifies in each row if the "profile picture" text exists
     for index, row in df.iterrows():
         if "profile picture" in str(row['FOLLOWING']).lower():
-            Following.append(row['FOLLOWING'])
+            following.append(row['FOLLOWING'])
         if "profile picture" in str(row['FOLLOWERS']).lower():
-            Followers.append(row['FOLLOWERS'])
+            followers.append(row['FOLLOWERS'])
     
     # Writes info on the results.txt
-    file.write(f'Users you Follow: {len(Following)}\n')
-    file.write(f'Users Following you: {len(Followers)}\n')
+    file.write(f'Users you Follow: {len(following)}\n')
+    file.write(f'Users Following you: {len(followers)}\n')
     
-    # Obtains Mutual and No Corresponding
-    for i in range(len(Following)):
-        if Following[i] not in Followers:
-            NoMatching.append(Following[i])
-        for j in range(len(Followers)):
-            if Following[i] == Followers[j]:
-                Mutual.append(Followers[j])
+    # Obtains mutual and No Corresponding
+    for i in range(len(following)):
+        if following[i] not in followers:
+            no_matching.append(following[i])
+        for j in range(len(followers)):
+            if following[i] == followers[j]:
+                mutual.append(followers[j])
                 break
     
-    file.write(f'Mutual Followers: {len(Mutual)}\n')
+    file.write(f'mutual followers: {len(mutual)}\n')
     file.write('No Corresponding Followers:\n')
     
     # Cleans the "'s profile picture" text in each
-    Usernames = [user.replace("'s profile picture", "") for user in NoMatching]
+    Usernames = [user.replace("'s profile picture", "") for user in no_matching]
 
     for k, user in enumerate(Usernames):
         file.write(f"{k + 1}. {user}\n")
